e577fb90b70e1a554bc6a263b44db07074a4ca50
haoa2 <humbertowoody@gmail.com>
2016-08-14 11:55:28 -0600

    Added Pictures and finished the Pastillero.ino file

--- Modules/Pastillero/Pastillero.ino
+++ Modules/Pastillero/Pastillero.ino
@@ -2,8 +2,8 @@
 
 #define COLS 2
 #define ROWS 3
-#define RED_LED 0
-#define GREEN_LED   1
+#define RED_LED 1
+#define GREEN_LED   0
 
 SoftwareSerial xbee(2, 3);
 
@@ -11,17 +11,19 @@ byte MOTOR_PIN = 8;
 byte BUZZER = 9;
 
 byte LEDS[ROWS][COLS] = {
-  {11,10}, // First compartment
-  {13,12}, // Second compartment
-  {4,5}    // Third compartment
+  {13,12}, // First compartment
+  {11,10}, // Second compartment
+  {5,4}    // Third compartment
 };
 
 char message;
 
 boolean now = false;
 
+boolean trigger = false;
+
 unsigned long previousMillis;
-long interval = 1000;
+long interval = 1500;
 int compartimiento;
 boolean alertOn = false;
 
@@ -65,8 +67,11 @@ void loop() {
       break;
     case 'c':
       now = false;
+      apagarTodo();
       break;
     default:
+      now = false;
+      apagarTodo();
       break;
   }
 
@@ -75,10 +80,10 @@ void loop() {
     
     if(!alertOn){
       // Turn on the Alarm
-      //digitalWrite(MOTOR_PIN, HIGH);
+      digitalWrite(MOTOR_PIN, HIGH);
       /* You need to give some time for the motor to begin running */
-      //delay(120);
-      //digitalWrite(BUZZER, HIGH);
+      delay(120);
+      digitalWrite(BUZZER, HIGH);
       encenderCompartimiento(compartimiento);
       alertOn = true;
     } else {
@@ -100,19 +105,24 @@ void loop() {
 void encenderCompartimiento(int c)
 {
   c = c-1;
-//  for(int i = 0; i < ROWS; i++)
-//  {
-//    digitalWrite(LEDS[i][RED_LED],HIGH);
-//    delay(500);
-//  }
-//  
-//  digitalWrite(LEDS[c][GREEN_LED], HIGH);
-//  delay(500);
-//  digitalWrite(LEDS[c][RED_LED], LOW);
-//  delay(500);
-  digitalWrite(LEDS[c][GREEN_LED],HIGH);
-  delay(1000);
-  digitalWrite(LEDS[c][RED_LED],HIGH);
-  delay(1000);
+  for(int i = 0; i < ROWS; i++)
+  {
+    digitalWrite(LEDS[i][RED_LED],HIGH);
+  }
+  
+  digitalWrite(LEDS[c][GREEN_LED], HIGH);
+  digitalWrite(LEDS[c][RED_LED], LOW);
 }
 
+void apagarTodo(){
+  digitalWrite(MOTOR_PIN, LOW);
+  digitalWrite(BUZZER, LOW);
+  // Turn off all LEDs
+  for(int i = 0; i < ROWS; i++)
+  {
+    for(int j = 0; j < COLS; j++)
+    {
+      digitalWrite(LEDS[i][j], LOW);
+    }
+  }
+}
