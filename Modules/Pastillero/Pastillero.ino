00b30628c9b8ec33984c28af1f6338ed97ca264d
haoa2 <humbertowoody@gmail.com>
2016-08-14 10:34:27 -0600

    LED control

--- Modules/Pastillero/Pastillero.ino
+++ Modules/Pastillero/Pastillero.ino
@@ -1,9 +1,20 @@
 #include <SoftwareSerial.h>
 
+#define COLS 2
+#define ROWS 3
+#define RED_LED 0
+#define GREEN_LED   1
+
 SoftwareSerial xbee(2, 3);
 
 byte MOTOR_PIN = 8;
 byte BUZZER = 9;
+
+byte LEDS[ROWS][COLS] = {
+  {11,10}, // First compartment
+  {13,12}, // Second compartment
+  {4,5}    // Third compartment
+};
 
 char message;
 
@@ -11,7 +22,7 @@ boolean now = false;
 
 unsigned long previousMillis;
 long interval = 1000;
-
+int compartimiento;
 boolean alertOn = false;
 
 void setup() {
@@ -21,6 +32,14 @@ void setup() {
 
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
+  
+  for(int i = 0; i < ROWS; i++)
+  {
+    for(int j = 0; j < COLS; j++)
+    {
+      pinMode(LEDS[i][j], OUTPUT);
+    }
+  }
 }
 
 void loop() {
@@ -32,9 +51,22 @@ void loop() {
   }
 
   switch(message){
-    case 'a':
-      //time to take a pill
+    case '1':
+      compartimiento = 1;
       now = true;
+      break;
+    case '2':
+      compartimiento = 2;
+      now = true;
+      break;
+    case '3':
+      compartimiento = 3;
+      now = true;
+      break;
+    case 'c':
+      now = false;
+      break;
+    default:
       break;
   }
 
@@ -42,16 +74,45 @@ void loop() {
     previousMillis = millis();
     
     if(!alertOn){
-      digitalWrite(BUZZER, HIGH);
-      digitalWrite(MOTOR_PIN, HIGH);
-
+      // Turn on the Alarm
+      //digitalWrite(MOTOR_PIN, HIGH);
+      /* You need to give some time for the motor to begin running */
+      //delay(120);
+      //digitalWrite(BUZZER, HIGH);
+      encenderCompartimiento(compartimiento);
       alertOn = true;
     } else {
-      digitalWrite(BUZZER, LOW);
       digitalWrite(MOTOR_PIN, LOW);
-
+      digitalWrite(BUZZER, LOW);
+      // Turn off all LEDs
+      for(int i = 0; i < ROWS; i++)
+      {
+        for(int j = 0; j < COLS; j++)
+        {
+          digitalWrite(LEDS[i][j], LOW);
+        }
+      }
       alertOn = false;
     }
   }
+}
+
+void encenderCompartimiento(int c)
+{
+  c = c-1;
+//  for(int i = 0; i < ROWS; i++)
+//  {
+//    digitalWrite(LEDS[i][RED_LED],HIGH);
+//    delay(500);
+//  }
+//  
+//  digitalWrite(LEDS[c][GREEN_LED], HIGH);
+//  delay(500);
+//  digitalWrite(LEDS[c][RED_LED], LOW);
+//  delay(500);
+  digitalWrite(LEDS[c][GREEN_LED],HIGH);
+  delay(1000);
+  digitalWrite(LEDS[c][RED_LED],HIGH);
+  delay(1000);
 }
 
